13ec17173433a367ddfdd75eafaa121588f7e368
netbrum <netbrum@tutanota.com>
2025-03-15 11:33:14 +0100

    Add comment guards

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ export const BOT_MENTION = "@netbrum ";
 export default (app: Probot) => {
   app.on("issue_comment.created", async (context) => {
     const comment = context.payload.comment.body;
+    const isPr = context.payload.issue.pull_request;
+
+    if (context.isBot || !isPr || !comment.startsWith(BOT_MENTION)) return;
 
     const command = getCommand(comment);
     const response = context.issue({ body: getResponse(command) });
