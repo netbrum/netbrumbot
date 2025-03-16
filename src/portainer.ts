24efcc5b4145e24483d56758442c41b109f3b279
netbrum <netbrum@tutanota.com>
2025-03-16 23:36:59 +0100

    Add getStacks method

--- src/portainer.ts
+++ src/portainer.ts
@@ -1,15 +1,17 @@
-const HOST = "http://127.0.0.1:9000"
-const ENVIRONMENT_ID = 1;
+// const HOST = "http://192.168.20.100:9000"
+// const ENDPOINT_ID = 1;
+
+type Method = "GET" | "POST" | "DELETE";
 
 export class PortainerClient {
   token: string
   host: string
-  environmentId: number
+  endpointId: number
 
-  constructor(token: string, host: string, environmentId: number) {
+  constructor(token: string, host: string, endpointId: number) {
     this.token = token;
     this.host = host;
-    this.environmentId = environmentId;
+    this.endpointId = endpointId;
   }
 
   static async getToken(host: string, username: string, password: string): Promise<string> {
@@ -26,9 +28,7 @@ export class PortainerClient {
     return jwt;
   }
 
-  async request(e: string, body?: object, headers?: HeadersInit): Promise<unknown> {
-    const [method, endpoint] = e.split(" ");
-
+  async request(method: Method, endpoint: string, body?: object, headers?: HeadersInit): Promise<Response> {
     const response = await fetch(`${this.host}/api${endpoint}`, {
       method,
       headers: {
@@ -38,6 +38,17 @@ export class PortainerClient {
       body: JSON.stringify(body)
     });
 
-    return response.json();
+    return response;
+  }
+
+  async getStacks(): Promise<any> {
+    // The EndpointID filter parameter seemingly does not work with swarm stacks..?
+    // https://github.com/portainer/portainer/blob/e1f9b69cd562538e46ce2d7d5a4a32851007f37e/api/http/handler/stacks/stack_list.go#L126-L135
+    //
+    // We just filter it ourselves.
+    const response = await this.request("GET", "/stacks");
+    const stacks: any[] = await response.json();
+
+    return stacks.filter((stack) => stack.EndpointId === this.endpointId);
   }
 }
